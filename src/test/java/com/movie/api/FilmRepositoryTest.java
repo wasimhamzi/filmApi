f1a4e3ba47c799990a1692a4156fd6acf9be1dfd
wasim HAMZI <hamziwasim@gmail.com>
2022-04-26 23:01:08 +0200

    remove unused imports

--- src/test/java/com/movie/api/FilmRepositoryTest.java
+++ src/test/java/com/movie/api/FilmRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 @DataJpaTest
 public class FilmRepositoryTest {
